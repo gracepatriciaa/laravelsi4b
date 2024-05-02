f7dd0261fc3fb9553a8da36f2e3a7ac40eb79b10
gracepatriciaa <gracepatricia_2226240079@mhs.mdp.ac.id>
2024-05-02 15:14:26 +0700

    laravel1-2

---
+++ grace1-app/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+use App\Http\Controllers\FakultasController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('about', function () {
+    return 'Halaman About';
+});
+
+Route::get('profile', function () {
+    return view('profile');
+});
+
+// route dgn parameter 
+Route::get('welcome/{salam}', function($salam) {
+    // return 'Selamat '. $salam;
+    return view ('salam') -> with('viewsalam', $salam);
+});
+
+// Route tanpa parameter 
+// terdapat array $list
+Route::get('listdata',function () {
+    $list = ["Sistem Informasi", "Informatika", "Manajemen"];
+    $listmhs = [
+        ["npm" => "001", "nama" => "ahmad"],
+        ["npm" => "002", "nama" => "budi"]
+    ];
+    return view('listprodi') 
+        -> with('viewlist', $list)
+        -> with('viewmhs', $listmhs);
+});
+
+Route::resource('fakultas', FakultasController::class);
