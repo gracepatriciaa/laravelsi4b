2e77daa28492a87c07c2991e918efb0dd8c50646
gracepatriciaa <gracepatricia_2226240079@mhs.mdp.ac.id>
2024-05-21 17:03:26 +0700

    tgl21mei

--- grace1-app/resources/views/fakultas/index.blade.php
+++ grace1-app/resources/views/fakultas/index.blade.php
@@ -1,18 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>UMDP</h1>
+@extends('layout.main')
+
+@section('title', 'Fakultas')
+
+@section('content')
+   {{-- <h1>UMDP</h1>
     <h2>Fakultas</h2>
     <ul>
         @foreach ($fakultas as $item)
             <li> {{$item["nama"]}} {{$item["singkatan"]}}</li>
         @endforeach
-    </ul>
-</body>
-</html>
+    </ul> --}}
+<div class="row">
+    <div class="col-lg-6 grid-margin stretch-card">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">Fakultas</h4>
+            <p class="card-description">
+              Add class <code>.table</code>
+            </p>
+            <div class="table-responsive">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>Nama Fakultas</th>
+                    <th>Singkatan</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @foreach ($fakultas as $item)
+                    <tr> 
+                        <td>{{$item["nama"]}} </td>
+                        <td>{{$item["singkatan"]}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+ @endsection
--- grace1-app/resources/views/prodi/index.blade.php
+++ grace1-app/resources/views/prodi/index.blade.php
@@ -1,18 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Program Studi</title>
-</head>
-<body>
-    <h1>UMDP</h1>
-    <h2>Program Studi</h2>
+@extends('layout.main')
+
+@section('title', 'Prodi')
+
+@section('content')
+   {{-- <h1>UMDP</h1>
+    <h2>Fakultas</h2>
     <ul>
-        @foreach ($prodi as $item)
-            <li> {{$item["nama"]}} {{$item["singkatan"]}} {{$item["fakultas"] ["nama"]}}</li>
+        @foreach ($fakultas as $item)
+            <li> {{$item["nama"]}} {{$item["singkatan"]}}</li>
         @endforeach
-    </ul>
-</body>
-</html>
+    </ul> --}}
+<div class="row">
+    <div class="col-lg-12 grid-margin stretch-card">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">Prodi</h4>
+            <p class="card-description">
+              Add class <code>.table</code>
+            </p>
+            <div class="table-responsive">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>Nama Prodi</th>
+                    <th>Singkatan</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @foreach ($prodi as $item)
+                    <tr> 
+                        <td>{{$item["nama"]}} </td>
+                        <td>{{$item["singkatan"]}}</td>
+                        <td>{{$item["fakultas"] ["nama"]}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+ @endsection
